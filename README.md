2c7253538714a2ccb1d63293ccc26e85e8bb2f46
yuchans87 <yuichi@news2u.co.jp>
2015-04-17 19:27:23 +0900

    docker build v0.1

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # git-docker
 Github Docker Test 2015-04-16
+
+Usage:
+sudo yum install -y git docker nginx
+sudo service docker start
+sudo docker pull yuchans87/amazn:v0.1
+mkdir DIR
+cd DIR/
+git clone git@github.com:yuchans87/git-docker.git
+
+sudo docker build -t REPO:TAG .
+sudo docker run -d -p 22 -p 80 REPO:TAG
+
+/bin/sh ./nginx_docker_port_setting.sh
--- nginx_docker_port_setting.sh
+++ nginx_docker_port_setting.sh
@@ -7,6 +7,11 @@ CONTAINER_ID=`sudo docker ps|tail -1|awk '{print $1}'`
 
 DOCKER_NGINX_PORT=`sudo docker inspect -f '{{(index (index .NetworkSettings.Ports "80/tcp") 0).HostPort}}' $CONTAINER_ID`
 
+NGINX_STATUS=`sudo sh -c "service nginx status"` > /dev/null
+if [ $? -ne 0 ]; then
+  sudo sh -c "service nginx start"
+fi
+
 if [ $DOCKER_NGINX_PORT ]; then
   echo $DOCKER_NGINX_PORT
   sudo sh -c "sed -e 's/PORT/$DOCKER_NGINX_PORT/g' $TEMPLATE_CONF > $CONF_FILE"
