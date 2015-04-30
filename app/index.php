4919dda237ef127d44134a8338c1d93677f7f0f4
yuchans87 <yuichi@news2u.co.jp>
2015-04-30 19:02:09 +0900

    _test change file index.php test3

--- app/index.php
+++ app/index.php
@@ -2,15 +2,12 @@
 $LOG_FILE = '/var/log/payload/hook.log';
 $SECRET_KEY = '7Aar9SRcV6PKy';
 
-if ( isset($_GET['key'])&& $_GET['key'] === $SECRET_KEY)
+if ( isset($_GET['key'])&& $_GET['key'] === $SECRET_KEY )
 {
   $json = file_get_contents('php://input');
   $params = json_decode($json, true);
-  if ($payload['ref'] === 'refs/heads/master')
-  {
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled; ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
-  }
+  error_log(print_r($params, true), 3, '/var/log/payload/hook.log');
 } else {
-    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
+  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 }
 ?>
