e53ad1964a55ce101d293c5dce9f4e87c380f3eb
yuchans87 <yuichi@news2u.co.jp>
2015-04-30 19:22:15 +0900

    _test change file index.php test4

--- app/index.php
+++ app/index.php
@@ -1,13 +1,18 @@
 <?php
 $LOG_FILE = '/var/log/payload/hook.log';
-$SECRET_KEY = '7Aar9SRcV6PKy';
+$secret = '7Aar9SRcV6PKy';
 
-if ( isset($_GET['key'])&& $_GET['key'] === $SECRET_KEY )
-{
-  $json = file_get_contents('php://input');
-  $params = json_decode($json, true);
-  error_log(print_r($params, true), 3, '/var/log/payload/hook.log');
-} else {
+$headers = getallheaders();
+$hubSignature = $headers['X-Hub-Signature'];
+
+list($algo, $hash) = explode('=', $hubSignature, 2);
+
+$payload = file_get_contents('php://input');
+
+$payloadHash = hash_hmac($algo, $payload, $secret);
+
+if ( $hash !== $payloadHash ) {
   file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
-}
+} else {
+  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." OK access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 ?>
