b3c08b89fab4572edf1e3947fe6770f520a388de
yuchans87 <yuichi@news2u.co.jp>
2015-04-30 19:32:26 +0900

    _test change file index.php json_decode test2

--- app/index.php
+++ app/index.php
@@ -8,12 +8,13 @@ $hubSignature = $headers['X-Hub-Signature'];
 list($algo, $hash) = explode('=', $hubSignature, 2);
 
 $payload = file_get_contents('php://input');
+$params = json_decode($payload, true);
 
 $payloadHash = hash_hmac($algo, $payload, $secret);
 
 if ( $hash !== $payloadHash ) {
   file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 } else {
-  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." OK access: ".$payload['head_commit']['message']." ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
+  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." OK access: ".$params['head_commit']['message']." ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 }
 ?>
