39e2b5576580099b27902e7cd28e5225ce2c72bb
yuchans87 <yuichi@news2u.co.jp>
2015-04-30 19:28:35 +0900

    _test change file index.php commit_message test

--- app/index.php
+++ app/index.php
@@ -14,6 +14,6 @@ $payloadHash = hash_hmac($algo, $payload, $secret);
 if ( $hash !== $payloadHash ) {
   file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 } else {
-  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." OK access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
+  file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." OK access: ".$payload['head_commit']['message']." ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
 }
 ?>
